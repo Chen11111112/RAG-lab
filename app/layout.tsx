8af3e05c8e6613f93fdcccd3d62a57f14d063604
Harry Chen <harrychen@HarrydeMacBook-Air.local>
2026-07-14 13:22:20 +0800

    local-rag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
-export default function Layout({children}: 
-  {children: React.ReactNode;}){ // Prop
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <>
-      <h1>我是Layout</h1>
-      <h2>你也可以叫我NavBar、SideBar</h2>
-      
-      {children}
+    <html lang="zh-Hant">
+      <body>
+        <h1>我是Layout</h1>
+        <h2>你也可以叫我NavBar、SideBar</h2>
 
-      <footer>@HyC 版權所有</footer>
-    </>
+        {children}
+
+        <footer>@HyC 版權所有</footer>
+      </body>
+    </html>
   )
 }
-// REACT
-// Components -> DOM Node
